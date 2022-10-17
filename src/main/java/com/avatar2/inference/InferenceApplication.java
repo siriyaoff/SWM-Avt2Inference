079120fa5d41fd4a3c5127d402ccf689f6be7243
siriyaoff <hwy16016@gmail.com>
2022-10-18 02:50:36 +0900

    BOILER PLATE

---
+++ src/main/java/com/avatar2/inference/InferenceApplication.java
@@ -0,0 +1,13 @@
+package com.avatar2.inference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avatar2/inference/InferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avatar2.inference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
