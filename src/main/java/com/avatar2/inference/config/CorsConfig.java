2e1eca449a1bb5124aa25391b48f2aaf9dff3360
siriyaoff <hwy16016@gmail.com>
2022-10-26 15:07:47 +0900

    feat(API): add CorsConfig

---
+++ src/main/java/com/avatar2/inference/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.avatar2.inference.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                        );
+    }
+}
