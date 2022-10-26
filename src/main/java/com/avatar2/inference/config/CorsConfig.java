787639330d900485ab67ca30079bc8e5af1cf380
siriyaoff <hwy16016@gmail.com>
2022-10-26 15:13:31 +0900

    feat(API): allow all origins in CorsConfig

--- src/main/java/com/avatar2/inference/config/CorsConfig.java
+++ src/main/java/com/avatar2/inference/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
