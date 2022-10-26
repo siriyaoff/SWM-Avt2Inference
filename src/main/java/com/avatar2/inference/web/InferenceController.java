9d58d0f8fdfc8c98398e7f14b1754f4181ac529a
siriyaoff <hwy16016@gmail.com>
2022-10-26 17:20:26 +0900

    feat(API): add '/inferencetest'

--- src/main/java/com/avatar2/inference/web/InferenceController.java
+++ src/main/java/com/avatar2/inference/web/InferenceController.java
@@ -11,10 +11,8 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import java.io.*;
@@ -80,22 +78,65 @@ public class InferenceController {
         return inferenceResponseDto;
     }
 
-    @GetMapping("/test")
-    public String test() throws IOException, InterruptedException {
+    @PostMapping("/inferencetest")
+    public InferenceResponseDto inference(@RequestPart(value = "psd") MultipartFile psd) throws IOException, InterruptedException, FileNotFoundException {
         InferenceResponseDto inferenceResponseDto = null;
-
-        // read psd as File, convert to multipartfile
         try {
-            File file = new File("/Users/yanghyowon/Downloads/tpsd.psd");
+            Path path = Paths.get("/home/ec2-user").resolve("tpsd.psd");
+            psd.transferTo(path);
+
+            InferenceController.processBuilder();
+
+            ArrayList<PElem> coord, eyelash;
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject, jsonObject1;
+            JSONArray pointArray;
+
+            Reader reader = new FileReader("/home/ec2-user/coord.json");
+            jsonObject = (JSONObject) parser.parse(reader);
+            pointArray = (JSONArray) parser.parse(jsonObject.get("Point").toString());
+            coord = new ArrayList<>();
+            for (Object obj : pointArray) {
+                jsonObject1 = (JSONObject) obj;
+                coord.add(PElem.builder()
+                        .id((String) jsonObject1.get("id"))
+                        .x((String) jsonObject1.get("x"))
+                        .y((String) jsonObject1.get("y"))
+                        .build());
+            }
+
+            reader = new FileReader("/home/ec2-user/eyelash.json");
+            jsonObject = (JSONObject) parser.parse(reader);
+            pointArray = (JSONArray) parser.parse(jsonObject.get("Point").toString());
+            eyelash = new ArrayList<>();
+            for (Object obj : pointArray) {
+                jsonObject1 = (JSONObject) obj;
+                eyelash.add(PElem.builder()
+                        .id((String) jsonObject1.get("id"))
+                        .x((String) jsonObject1.get("x"))
+                        .y((String) jsonObject1.get("y"))
+                        .build());
+            }
+
+            File file = new File("/home/ec2-user/iris.png");
             FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
             InputStream input = new FileInputStream(file);
             OutputStream os = fileItem.getOutputStream();
             IOUtils.copy(input, os);
             MultipartFile iris = new CommonsMultipartFile(fileItem);
+            inferenceResponseDto = new InferenceResponseDto(coord, eyelash, iris, "");
         } catch (Exception e) {
-            e.printStackTrace();
+            String err = e.getStackTrace()[0].toString();
+            return new InferenceResponseDto(null, null, null, err);
         }
+        return inferenceResponseDto;
+    }
 
+    @GetMapping("/test")
+    public InferenceResponseDto test() throws IOException, InterruptedException {
+        InferenceResponseDto inferenceResponseDto = null;
+
+        // read psd as File, convert to multipartfile
         try {
             ArrayList<PElem> coord, eyelash;
             JSONParser parser = new JSONParser();
@@ -137,19 +178,47 @@ public class InferenceController {
             MultipartFile iris = new CommonsMultipartFile(fileItem);
             inferenceResponseDto = new InferenceResponseDto(coord, eyelash, iris, "");
 
-
-
-            Path path = Paths.get("/Users/yanghyowon/Downloads").resolve("serveriris.png");
-            iris.transferTo(path);
+            return inferenceResponseDto;
 
         } catch (Exception e) {
             inferenceResponseDto=new InferenceResponseDto(null, null, null, e.getStackTrace()[0].toString());
+            return inferenceResponseDto;
         }
-        if(inferenceResponseDto.getErr()=="")
-            return inferenceResponseDto.getCoord().get(1).toString();
-        else return inferenceResponseDto.getErr();
     }
 
+    @GetMapping("/json")
+    public String json() throws IOException, ParseException {
+        ArrayList<PElem> coord, eyelash;
+        JSONParser parser = new JSONParser();
+        JSONObject ob, elemob;
+        JSONArray point;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Reader reader = new FileReader("/Users/yanghyowon/Downloads/testPoint.json");
+        ob = (JSONObject) parser.parse(reader);
+        point = (JSONArray) parser.parse(ob.get("Point").toString());
+        coord = new ArrayList<>();
+        for (Object obj : point) {
+            elemob = (JSONObject) obj;
+            coord.add(PElem.builder()
+                    .id((String) elemob.get("id"))
+                    .x((String) elemob.get("x"))
+                    .y((String) elemob.get("y"))
+                    .build());
+        }
+
+        File file = new File("/Users/yanghyowon/Downloads/iris.png");
+        FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+        InputStream input = new FileInputStream(file);
+        OutputStream os = fileItem.getOutputStream();
+        IOUtils.copy(input, os);
+        MultipartFile iris = new CommonsMultipartFile(fileItem);
+        InferenceResponseDto inferenceResponseDto = new InferenceResponseDto(coord, coord, iris, "");
+
+        String res = objectMapper.writeValueAsString(inferenceResponseDto.getCoord());
+
+        return res;
+    }
     public static void processBuilder() throws IOException, InterruptedException {
         String homeDir = System.getProperty("user.home");
         ProcessBuilder builder = new ProcessBuilder();
